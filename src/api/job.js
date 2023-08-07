de50fb0553594425b784d1cbf1e75537c31a8b43
ilhamsyah13 <ilham1304syah@gmail.com>
2023-08-07 10:30:01 +0700

    commit

--- src/api/job.js
+++ src/api/job.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/api/recruitments/positions";
+const BASE_URL = "http://localhost:3000/api/recruitment/positions";
 
 const list = async (payload) => {
   try {
